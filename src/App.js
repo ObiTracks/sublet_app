57ceee09de09e5f9fccda1bb6e20422dcae05087
ObiTracks <mo.ihej17@gmail.com>
2020-02-16 11:51:39 -0500

    Added titles and descriptions etc. and cleaned up

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Particles from 'react-particles-js';
 import Card from './Components/Card/Card';
 import Nav from './Components/Nav/Nav';
 import data from './data/data.json';
+import logo from '../src/static/logo.png';
 
 const particleOptions = {
   particles: {
@@ -29,13 +30,16 @@ function App() {
         <Particles params={particleOptions} style={{backgroundColor: "navy", position: "absolute", width: '100%', height: "10px"}}/>
 
         <div className="Intro">
+          <p className="IntroTitle">SUBLTR</p>
+          {/* <img className="IntroTitle" src={logo}></img> */}
             <div className="search">
               <input></input>
             </div>
+          <p className="IntroSlogan">Stop searching for student short-term housing on facebook. We both know its not fun</p>
         </div>
 
         <h1 className="cardsTitle">Browse Offerings</h1>
-        <p className="cardsDescription">This is a platform designed to find housing information for students to find apartment housing
+        <p className="cardsDescription">This is a platform designed to find short-term housing information for students to find apartment housing
           offered by people like students who are leaving for co-op and would have to continue paying while
           not living at their residence. It provides simple way to see options posted on facebook kijiji etc.
           and for people to find the original webpage the information was scraped from.
