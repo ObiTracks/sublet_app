5e0e02cf1217960b7aa8dc99e0dbc000a1318356
ObiTracks <mo.ihej17@gmail.com>
2020-02-16 11:06:03 -0500

    Added a site description and title. Will add the logo to the top corner

--- src/App.js
+++ src/App.js
@@ -34,6 +34,12 @@ function App() {
             </div>
         </div>
 
+        <h1 className="cardsTitle">Browse Offerings</h1>
+        <p className="cardsDescription">This is a platform designed to find housing information for students to find apartment housing
+          offered by people like students who are leaving for co-op and would have to continue paying while
+          not living at their residence. It provides simple way to see options posted on facebook kijiji etc.
+          and for people to find the original webpage the information was scraped from.
+        </p>
         <div className="Cards">
             {data.kijiji.map(item => {
               return <Card link={item.URL} price={item.price} location={item.Location}/>
